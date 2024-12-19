cc66a9ae0e4f4eece9466fe42d1b79c68bbd9d96
LuisGHA <100371953+LuisGHA@users.noreply.github.com>
2024-12-18 19:07:03 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paquete_mooc',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Descripción breve del paquete',
+    author='LuisGH',
+    author_email='launghv@comunidad.unam.mx',
+    url='https://github.com/LuisGHA/paquete_mooc.git',
+    install_requires=[
+        # Lista de dependencias necesarias
+    ],
+)
